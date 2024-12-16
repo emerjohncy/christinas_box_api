ad78ab3b22a0ea036920c98a9572e5835f3fb287
emerjohncy <nionesemerjohn@gmail.com>
2024-12-16 23:35:43 +0800

    created update method for categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CategoriesController < ApplicationController
-  before_action :get_category, only: [:show]
+  before_action :get_category, only: [:show, :update]
 
   # GET /categories
   def index
@@ -19,6 +19,15 @@ class Api::V1::CategoriesController < ApplicationController
 
     if @category.save
       render json: { status: 'Success', message: 'Category created successfully.', data: @category }, status: :created
+    else
+      render json: { status: 'Error', message: @category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /categories/1
+  def update
+    if @category.update(category_params)
+      render json: { status: 'Success', message: 'Category updated successfully.', data: @category }, status: :updated
     else
       render json: { status: 'Error', message: @category.errors.full_messages }, status: :unprocessable_entity
     end
