9b0a43bcd0579448c7785ff235baac4c94255de7
emerjohncy <nionesemerjohn@gmail.com>
2024-12-16 22:27:06 +0800

    add show method in categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,9 +1,21 @@
 class Api::V1::CategoriesController < ApplicationController
-    # GET /categories
+  before_action :get_category, only: [:show]
+
+  # GET /categories
   def index
     @categories = Category.all
 
     render json: @categories
   end
 
+  # GET /categories/1
+  def show
+    render json: @category
+  end
+
+  private
+
+  def get_category
+    @category = Category.find(params[:id])
+  end
 end
