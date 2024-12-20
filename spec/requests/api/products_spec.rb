0fc737e2feb91557d9e288f70b46acd4daad911a
emerjohncy <nionesemerjohn@gmail.com>
2024-12-20 11:05:47 +0800

    added rspec test on categories and products controllers if destroy method fails

--- spec/requests/api/products_spec.rb
+++ spec/requests/api/products_spec.rb
@@ -341,6 +341,19 @@ RSpec.describe "Products", type: :request do
         expect(response.body).to include("Product was deleted successfully.")
         expect(Category.exists?(category1.id)).to be true
       end
+
+      context "when deletion fails" do
+        before do
+          allow_any_instance_of(Product).to receive(:destroy).and_return(false)
+          delete api_v1_category_product_path(product1.category_id.to_s, product1.id.to_s)
+        end
+  
+        it "returns an error response" do
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(response.body).to include("Error")
+          expect(response.body).to include("Failed to delete product")
+        end
+      end
     end
   end
 end
